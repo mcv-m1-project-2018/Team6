554777766bc0ef27f98329911489f747d20e83bd
Oscar Mañas <oscar@restb.ai>
2018-10-07 11:36:38 +0200

    Improve dataset split considering lightness

--- traffic_signs/dataset_split.py
+++ traffic_signs/dataset_split.py
@@ -2,38 +2,92 @@
 import os
 import glob
 import argparse
+import shutil
 from collections import defaultdict
 
 import numpy as np
+import imageio
 
 
-def main(args):
+def get_patch_lightness(img, mask, bbox):
+    tly, tlx, bry, brx = bbox
+    img_patch = img[tly:bry,tlx:brx]
+    mask_patch = mask[tly:bry,tlx:brx]
+    mask_patch = np.repeat(mask_patch[:,:,np.newaxis], 3, axis=2)
+    pixels = img_patch[np.nonzero(mask_patch)].reshape((-1, 3))
+    
+    norm_rgb = pixels / 255
+    c_max = norm_rgb.max(axis=1)
+    c_min = norm_rgb.min(axis=1)
+    L = (c_max + c_min) / 2
+    
+    return np.mean(L)
+
+
+def split(image_files, mask_files, gt_files):
     images_per_class = defaultdict(list)
-    for image_file in sorted(glob.glob(os.path.join(args.data_path, '*.jpg'))):
-        name = os.path.splitext(os.path.split(image_file)[1])[0]
-        mask_file = os.path.join(args.data_path, 'mask/mask.{}.png'.format(name))
-        gt_file = os.path.join(args.data_path, 'gt/gt.{}.txt'.format(name))
+    lightness = {}
+    for image_file, mask_file, gt_file in zip(image_files, mask_files, gt_files):
+        img = imageio.imread(image_file)
+        mask = imageio.imread(mask_file)
         gts = [line.split(' ') for line in open(gt_file, 'r').read().splitlines()]
-        labels = [gt[4] for gt in gts]
-        label = np.random.choice(labels)
+        gt = gts[np.random.choice(range(len(gts)))]
+        bbox = np.round(list(map(int, map(float, gt[:4]))))
+        label = gt[4]
         images_per_class[label].append(image_file)
+        lightness[image_file] = get_patch_lightness(img, mask, bbox)
 
     train, val = [], []
     for label in sorted(images_per_class.keys()):
-        image_files = images_per_class[label]
-        np.random.shuffle(image_files)  # TODO: improve intra-class split
-        c = int(np.ceil(len(image_files) * args.train_pct))
-        train.extend(image_files[:c])
-        val.extend(image_files[c:])
-        print('{}: {}/{}'.format(label, c, len(image_files) - c))
+        class_images = images_per_class[label]
+        np.random.shuffle(class_images)
+        
+        # intra-class split considering lightness
+        class_images = sorted(class_images, key=lambda x: lightness[x])
+        bins = 10
+        bin_size = int(np.ceil(len(class_images)/bins))
+        c = int(np.ceil(bin_size * args.train_pct))
+        for i in range(0, len(class_images), bin_size):
+            train.extend(class_images[i:i+c])
+            val.extend(class_images[i+c:i+bin_size])
+            
+    return train, val
+
+
+def main(args):
+    image_files = glob.glob(os.path.join(args.data_path, '*.jpg'))
+    mask_files, gt_files = [], []
+    for image_file in image_files:
+        name = os.path.splitext(os.path.split(image_file)[1])[0]
+        mask_files.append(os.path.join(args.data_path, 'mask/mask.{}.png'.format(name)))
+        gt_files.append(os.path.join(args.data_path, 'gt/gt.{}.txt'.format(name)))
+    
+    train, val = split(image_files, mask_files, gt_files)
+    print('{}/{}'.format(len(train), len(val)))
 
     with open(os.path.join(args.output_path, 'train.txt'), 'w') as f:
         f.write('\n'.join(train))
-
-    with open(os.path.join(args.data_path, 'val.txt'), 'w') as f:
+        
+    with open(os.path.join(args.output_path, 'val.txt'), 'w') as f:
         f.write('\n'.join(val))
     
-    
+    #train_path = 'data/train_val/train'
+    #if not os.path.exists(train_path):
+    #    os.makedirs(train_path)
+    #for src in train:
+    #    dst = os.path.join(train_path, os.path.split(src)[1])
+    #    shutil.copyfile(src, dst)
+    #    print('copy: {} -> {}'.format(src, dst))
+    #    
+    #val_path = 'data/train_val/val'
+    #if not os.path.exists(val_path):
+    #    os.makedirs(val_path)
+    #for src in val:
+    #    dst = os.path.join(val_path, os.path.split(src)[1])
+    #    shutil.copyfile(src, dst)
+    #    print('copy: {} -> {}'.format(src, dst))
+        
+        
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type=str, default='data/train')
