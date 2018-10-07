8cbcbdc2bb1fbe3b6ab73a1aaa4cf6e205660a83
Oscar Mañas <oscar@restb.ai>
2018-10-07 12:40:12 +0200

    Add gt and mask to dataset split

--- traffic_signs/dataset_split.py
+++ traffic_signs/dataset_split.py
@@ -65,27 +65,45 @@ def main(args):
     train, val = split(image_files, mask_files, gt_files)
     print('{}/{}'.format(len(train), len(val)))
 
-    with open(os.path.join(args.output_path, 'train.txt'), 'w') as f:
-        f.write('\n'.join(train))
-        
-    with open(os.path.join(args.output_path, 'val.txt'), 'w') as f:
-        f.write('\n'.join(val))
-    
-    #train_path = 'data/train_val/train'
-    #if not os.path.exists(train_path):
-    #    os.makedirs(train_path)
-    #for src in train:
-    #    dst = os.path.join(train_path, os.path.split(src)[1])
-    #    shutil.copyfile(src, dst)
-    #    print('copy: {} -> {}'.format(src, dst))
+    #with open(os.path.join(args.output_path, 'train.txt'), 'w') as f:
+    #    f.write('\n'.join(train))
     #    
-    #val_path = 'data/train_val/val'
-    #if not os.path.exists(val_path):
-    #    os.makedirs(val_path)
-    #for src in val:
-    #    dst = os.path.join(val_path, os.path.split(src)[1])
-    #    shutil.copyfile(src, dst)
-    #    print('copy: {} -> {}'.format(src, dst))
+    #with open(os.path.join(args.output_path, 'val.txt'), 'w') as f:
+    #    f.write('\n'.join(val))
+    
+    train_path = 'data/train_val/train'
+    train_mask_path = 'data/train_val/train/mask'
+    train_gt_path = 'data/train_val/train/gt'
+    val_path = 'data/train_val/val'
+    val_mask_path = 'data/train_val/val/mask'
+    val_gt_path = 'data/train_val/val/gt'
+    for path in [train_path, train_mask_path, train_gt_path, val_path, val_mask_path, val_gt_path]:
+        if not os.path.exists(path):
+            os.makedirs(path)
+    
+    for src_image_file in train:
+        name = os.path.splitext(os.path.split(src_image_file)[1])[0]
+        src_mask_file = os.path.join('data/train/mask/mask.{}.png'.format(name))
+        src_gt_file = os.path.join('data/train/gt/gt.{}.txt'.format(name))
+        dst_image_file = os.path.join(train_path, os.path.split(src_image_file)[1])
+        dst_mask_file = os.path.join(train_mask_path, os.path.split(src_mask_file)[1])
+        dst_gt_file = os.path.join(train_gt_path, os.path.split(src_gt_file)[1])
+        shutil.copyfile(src_image_file, dst_image_file)
+        shutil.copyfile(src_mask_file, dst_mask_file)
+        shutil.copyfile(src_gt_file, dst_gt_file)
+        print('copy: {} -> {}'.format(src_image_file, dst_image_file))
+
+    for src_image_file in val:
+        name = os.path.splitext(os.path.split(src_image_file)[1])[0]
+        src_mask_file = os.path.join('data/train/mask/mask.{}.png'.format(name))
+        src_gt_file = os.path.join('data/train/gt/gt.{}.txt'.format(name))
+        dst_image_file = os.path.join(val_path, os.path.split(src_image_file)[1])
+        dst_mask_file = os.path.join(val_mask_path, os.path.split(src_mask_file)[1])
+        dst_gt_file = os.path.join(val_gt_path, os.path.split(src_gt_file)[1])
+        shutil.copyfile(src_image_file, dst_image_file)
+        shutil.copyfile(src_mask_file, dst_mask_file)
+        shutil.copyfile(src_gt_file, dst_gt_file)
+        print('copy: {} -> {}'.format(src_image_file, dst_image_file))
         
         
 if __name__ == '__main__':
