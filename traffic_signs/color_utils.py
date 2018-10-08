01641a8aaf3adb9a1e01ebbd1ec1421b874392ba
Oscar Mañas <oscar@restb.ai>
2018-10-08 08:24:02 +0200

    Merge reference_colors.py and color_ranges.py into color_utils.py

--- traffic_signs/color_utils.py
+++ traffic_signs/color_utils.py
@@ -15,20 +15,31 @@ from matplotlib import pyplot as plt
 
 def dominant_colors(img, mask, bbox, k=7, n=2):
     tly, tlx, bry, brx = bbox
-    
+
     img_patch = img[tly:bry,tlx:brx]
     mask_patch = mask[tly:bry,tlx:brx]
     mask_patch = np.repeat(mask_patch[:,:,np.newaxis], 3, axis=2)
     pixels = img_patch[np.nonzero(mask_patch)].reshape((-1, 3))
-    
+
     clt = KMeans(n_clusters=k)
     labels = clt.fit_predict(pixels)
-    
+
     c = Counter(labels)
     most_common, _ = zip(*c.most_common(n))
     most_common = list(most_common)
-    
+
     return clt.cluster_centers_[most_common].astype(np.uint8)
+
+
+def pixel_colors(img, mask, bbox):
+    tly, tlx, bry, brx = bbox
+
+    img_patch = img[tly:bry, tlx:brx]
+    mask_patch = mask[tly:bry, tlx:brx]
+    mask_patch = np.repeat(mask_patch[:, :, np.newaxis], 3, axis=2)
+    pixels = img_patch[np.nonzero(mask_patch)].reshape((-1, 3))
+
+    return pixels
 
 
 def worker(img_file):
@@ -45,21 +56,70 @@ def worker(img_file):
     return colors
 
 
-def reference_colors(images):    
+def reference_colors(images):
     with mp.Pool(8) as p:
         r = p.map(worker, images)
     rgb_colors = np.concatenate(r)
-            
+
     hsv_colors = []
     for rgb in rgb_colors:
         hsv = list(colorsys.rgb_to_hsv(*(rgb/255)))
         hsv[2] = 1
         hsv_colors.append(hsv)
-        
+
     clt = KMeans(n_clusters=3)
     labels = clt.fit_predict(hsv_colors)
-    
+
     return clt.cluster_centers_
+
+
+def hue_histograms(images):
+    with mp.Pool(8) as p:
+        r = p.map(worker, images)
+    rgb_colors = np.concatenate(r)
+
+    hsv_colors = []
+    for rgb in rgb_colors:
+        hsv = list(colorsys.rgb_to_hsv(*(rgb / 255)))
+        hsv[2] = 1
+        hsv_colors.append(hsv)
+
+    clt = KMeans(n_clusters=3)
+    labels = clt.fit_predict(hsv_colors)
+    for k in range(3):
+        color_list = [i for (i, v) in zip(hsv_colors, labels) if v == k]
+        H_list = [color[0]*255 for color in color_list]
+        fig = plt.figure(figsize=(6, 1), frameon=False)
+        plt.hist(H_list)
+        m = np.mean(H_list)
+        std = np.std(H_list)
+        print('Color {0}: H in [{1}, {2}]'.format(k, m-std/2, m+std/2))
+
+        plt.title(k)
+
+
+def rgb_histograms(images):
+    with mp.Pool(8) as p:
+        r = p.map(worker, images)
+    rgb_colors = np.concatenate(r)
+
+    clt = KMeans(n_clusters=3)
+    labels = clt.fit_predict(rgb_colors)
+    channel = ['R', 'G', 'B']
+    m = [0, 0, 0]
+    for k in range(3):
+        color_values = [i for (i, v) in zip(rgb_colors, labels) if v == k]
+        for col in range(3):
+            col_list = [color[col] for color in color_values]
+            m[col] = np.mean(col_list)
+            std = np.std(col_list)
+            fig = plt.figure(figsize=(6, 1), frameon=False)
+            plt.hist(col_list)
+            plt.title('{0}: Channel {1}'.format(k, channel[col]))
+            print('Channel {0}: values in [{1}, {2}]'.format(channel[col], m[col] - std, m[col] + std))
+
+        print(m)
+        plt.show()
 
 
 def show_colors(colors):
@@ -72,15 +132,17 @@ def show_colors(colors):
     ax.set_xticks([])
     ax.set_yticks([])
     ax.set_aspect('equal')
-    
+
 
 def main():
     images = glob.glob('data/train/*.jpg')
+
     ref_colors = reference_colors(images)
     [print('({:.4f}, {:.4f}, {:.4f})'.format(*c)) for c in ref_colors]
-    show_colors([colorsys.hsv_to_rgb(*hsv) for hsv in ref_colors])
-    plt.show()
-    
+
+    rgb_histograms(images)
+    hue_histograms(images)
+
 
 if __name__ == '__main__':
     main()
