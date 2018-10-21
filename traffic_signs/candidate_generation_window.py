34e0c7d8c124a9607d18131982a166cbfdfa28f7
Oscar Mañas <oscmansan@gmail.com>
2018-10-21 12:04:43 +0200

    Optimize template matching
    * Add clustering to compute box sizes
    
    * Optimize template matching
    
    * Add rgb2gray conversion + improve nms

--- traffic_signs/candidate_generation_window.py
+++ traffic_signs/candidate_generation_window.py
@@ -5,6 +5,7 @@ import numpy as np
 import imageio
 from skimage.measure import label, regionprops
 from skimage.transform import resize
+import cv2
 
 from matplotlib import pyplot as plt
 import matplotlib.patches as mpatches
@@ -52,14 +53,26 @@ def candidate_generation_window_ccl(im, pixel_candidates, eval_method):
     return window_candidates
 
 
+def is_contained(container, content):
+    return container[0] <= content[0] and \
+           container[1] <= content[1] and \
+           container[2] >= content[2] and \
+           container[3] >= content[3]
+
+
 def nms(bboxes, threshold=.4):
     # based on https://github.com/opencv/opencv/blob/master/modules/dnn/src/nms.inl.hpp
+
+    # sort bboxes by area descending
+    bboxes.sort(key=lambda x: (x[2]-x[0])*(x[3]-x[1]), reverse=True)
+
     indices = []
     for idx in range(len(bboxes)):
         keep = True
         for kept_idx in indices:
             overlap = bbox_iou(bboxes[idx], bboxes[kept_idx])
-            if overlap > threshold:
+            contained = is_contained(bboxes[idx], bboxes[kept_idx]) or is_contained(bboxes[kept_idx], bboxes[idx])
+            if contained or overlap > threshold:
                 keep = False
                 break
         if keep:
@@ -75,11 +88,22 @@ def _worker_template(x):
     window_candidates = []
     for template in we.TEMPLATES:
         template = resize(template, (box_h, box_w), preserve_range=True).astype(np.uint8)
-        for i in range(0, h-box_h, step):
-            for j in range(0, w-box_w, step):
-                bbox = [i, j, i+box_h, j+box_w]
-                if we.window_evaluation_template(masked_im, bbox, template, corr_thresh=0.7):
-                    window_candidates.append(bbox)
+
+        #for i in range(0, h-box_h, step):
+        #    for j in range(0, w-box_w, step):
+        #        bbox = [i, j, i+box_h, j+box_w]
+        #        if we.window_evaluation_template(masked_im, bbox, template, corr_thresh=0.7):
+        #            window_candidates.append(bbox)
+
+        # OpenCV implementation of template matching is a lot faster
+        res = cv2.matchTemplate(masked_im, template, method=cv2.TM_CCORR_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        tlx, tly = max_loc
+        score = max_val
+        if score > 0.8:
+            bbox = [tly, tlx, tly + box_h, tlx + box_w]
+            window_candidates.append(bbox)
+
     return window_candidates
 
 
@@ -98,7 +122,7 @@ def _worker_features(x):
 
 def sliding_window_par(im, pixel_candidates, eval_method, step=5, nms_threshold=.1):
     # scales = [(h, w) for h in range(180, 30-1, -50) for w in range(180, 30-1, -50)]
-    scales = [(88, 83), (144, 137), (194, 202), (45, 43), (113, 102), (67, 62)]
+    scales = [(217, 235), (181, 186), (140, 143), (164, 113), (116, 117), (100, 99), (119, 78), (82, 80), (66, 60), (45, 43)]
 
     if eval_method == 'features':
         worker = _worker_features
@@ -120,7 +144,7 @@ def sliding_window_par(im, pixel_candidates, eval_method, step=5, nms_threshold=
 def sliding_window_seq(im, pixel_candidates, eval_method, step=5, nms_threshold=.1):
     h, w = im.shape[:2]
     # scales = [(h, w) for h in range(180, 30-1, -50) for w in range(180, 30-1, -50)]
-    scales = [(88, 83), (144, 137), (194, 202), (45, 43), (113, 102), (67, 62)]
+    scales = [(217, 235), (181, 186), (140, 143), (164, 113), (116, 117), (100, 99), (119, 78), (82, 80), (66, 60), (45, 43)]
 
     # generate windows
     window_candidates = []
@@ -130,7 +154,7 @@ def sliding_window_seq(im, pixel_candidates, eval_method, step=5, nms_threshold=
                 for j in range(0, w-box_w, step):
                     bbox = [i, j, i+box_h, j+box_w]
                     # evaluate window (possible detection)
-                    if we.window_evaluation_rand(pixel_candidates, bbox):
+                    if we.window_evaluation_features(pixel_candidates, bbox):
                         window_candidates.append(bbox)
         elif eval_method == 'template':
             for template in we.TEMPLATES:
@@ -154,7 +178,7 @@ def sliding_window_seq(im, pixel_candidates, eval_method, step=5, nms_threshold=
 
 def visualize_boxes(pixel_candidates, window_candidates):
     fig, ax = plt.subplots(figsize=(10, 6))
-    ax.imshow(pixel_candidates * 255)
+    ax.imshow(pixel_candidates)
     for candidate in window_candidates:
         minr, minc, maxr, maxc = candidate
         rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr, fill=False, edgecolor='red', linewidth=2)
@@ -181,7 +205,8 @@ def switch_method(im, pixel_candidates, method):
 
 
 def candidate_generation_window(im, pixel_candidates, method):
-    window_candidates = switch_method(im, pixel_candidates, method)
+    im_gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+    window_candidates = switch_method(im_gray, pixel_candidates, method)
     return window_candidates
 
 
@@ -189,12 +214,14 @@ if __name__ == '__main__':
     import glob, os, time
     img_file = np.random.choice(glob.glob('data/train/*.jpg'))
     name = os.path.splitext(os.path.split(img_file)[1])[0]
-    im = imageio.imread(img_file, as_gray=True).astype(np.uint8)
-    mask = imageio.imread('data/train/mask/mask.{}.png'.format(name))
+    im = imageio.imread(img_file)
+    mask = imageio.imread('output/hsv_ranges/{}.png'.format(name))
 
     start = time.time()
-    window_candidates = candidate_generation_window(im, mask, 'ccl_template')
+    window_candidates = candidate_generation_window(im, mask, 'sw_template')
     end = time.time()
     print('time: {}'.format(end-start))
 
+    im_gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+    visualize_boxes(im_gray*mask, window_candidates)
     visualize_boxes(im, window_candidates)
--- traffic_signs/data_analysis.py
+++ traffic_signs/data_analysis.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 
 import imageio
 import numpy as np
+from sklearn.cluster import KMeans
 
 from evaluation.bbox_iou import bbox_iou
 
@@ -38,17 +39,18 @@ def compute_num_overlap(gts, img):
                 bboxB = list(map(float, gts[j][:4]))
                 print(img, bbox_iou(bboxA, bboxB))
 
+
 def height(bbox):
     tly, tlx, bry, brx = bbox
-    return bry-tly
+    return bry - tly
+
 
 def width(bbox):
     tly, tlx, bry, brx = bbox
-    return brx-tlx
+    return brx - tlx
 
 
-
-def main():
+def compute_metrics():
     class_frequency = defaultdict(int)
     size_per_class = defaultdict(list)
     form_factor_per_class = defaultdict(list)
@@ -78,8 +80,32 @@ def main():
         print("FORM FACTOR", "max", max(form_factor_per_class[clase]), "min", min(form_factor_per_class[clase]))
         print("FILLING RATIO", "max", max(filling_ratio_per_class[clase]), "min", min(filling_ratio_per_class[clase]))
         print("SIZE", "max", max(size_per_class[clase]), "min", min(size_per_class[clase]))
-    print("WIDTH", 'max', max(width_bbox), 'min', min(width_bbox), 'mean', np.mean(width_bbox), 'std', np.std(width_bbox))
-    print("HEIGHT", 'max', max(height_bbox), 'min', min(height_bbox), 'mean', np.mean(height_bbox), 'std', np.std(height_bbox))
+    print("WIDTH", 'max', max(width_bbox), 'min', min(width_bbox), 'mean', np.mean(width_bbox), 'std',
+          np.std(width_bbox))
+    print("HEIGHT", 'max', max(height_bbox), 'min', min(height_bbox), 'mean', np.mean(height_bbox), 'std',
+          np.std(height_bbox))
+
+
+def compute_box_sizes(k):
+    sizes = []
+    for gt_file in glob.glob('data/train/gt/gt.*.txt'):
+        gts = [line.split(' ') for line in open(gt_file, 'r').read().splitlines()]
+        for gt in gts:
+            bbox = np.round(list(map(int, map(float, gt[:4]))))
+            height = bbox[2] - bbox[0]
+            width = bbox[3] - bbox[1]
+            sizes.append([height, width])
+    sizes = np.array(sizes)
+
+    kmeans = KMeans(n_clusters=k)
+    kmeans.fit(sizes)
+    centroids = kmeans.cluster_centers_
+    return centroids
+
+
+def main():
+    sizes = compute_box_sizes(k=10)
+    print(sorted([(h, w) for h, w in sizes.round().astype(np.uint32)], key=lambda x: x[0] * x[1], reverse=True))
 
 
 if __name__ == '__main__':
