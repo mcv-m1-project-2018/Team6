d58a1a8e5a1a9b4bc3de67ba32ecb3cc93f87dd0
daniel fuentes <daniel.fuentesl@e-campus.uab.cat>
2018-10-24 23:24:28 +0200

    Add some similarity methods and descriptors

--- painting_retrieval/src/descriptors.py
+++ painting_retrieval/src/descriptors.py
@@ -1,3 +1,8 @@
+import cv2
+import numpy as np
+from sklearn.cluster import KMeans
+from collections import Counter
+
 def descriptor(image):
     """
     Extract descriptors of an image.
@@ -11,3 +16,45 @@ def descriptor(image):
     """
 
     pass
+
+def descriptor_hsv(image):
+    """
+    Extract descriptors of an image in HSV color space.
+
+    Args:
+        image (ndarray): (H x W x C) 3D array of type np.uint8 containing an image.
+
+    Returns:
+        ndarray: 1D array of type np.float32 containing image descriptors.
+
+    """
+
+    img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    hue_hist = cv2.calcHist([img_hsv],[0],None,[256],[0,256])
+    saturation_hist = cv2.calcHist([img_hsv],[1],None,[256],[0,256])
+    value_hist = cv2.calcHist([img_hsv],[2],None,[256],[0,256])
+
+    acum_hist = (hue_hist + saturation_hist + value_hist) / 3
+
+    return np.array(acum_hist, dtype=np.float32).ravel()
+
+def descriptor_rgb(image):
+    """
+    Extract descriptors of an image in RGB color space.
+
+    Args:
+        image (ndarray): (H x W x C) 3D array of type np.uint8 containing an image.
+
+    Returns:
+        ndarray: 1D array of type np.float32 containing image descriptors.
+
+    """
+
+    r_hist = cv2.calcHist([image],[0],None,[256],[0,256])
+    g_hist = cv2.calcHist([image],[1],None,[256],[0,256])
+    b_hist = cv2.calcHist([image],[2],None,[256],[0,256])
+
+    acum_hist = (r_hist + g_hist + b_hist) / 3
+
+    return np.array(acum_hist, dtype=np.float32).ravel()
--- painting_retrieval/src/similarity_measures.py
+++ painting_retrieval/src/similarity_measures.py
@@ -1,6 +1,41 @@
-def similarity(u, v):
+import cv2
+import numpy as np
+from sklearn.cluster import KMeans
+from collections import Counter
+
+
+def similarity_correlation(u, v):
     """
-    Compare descriptor vectors based on a similarity measure.
+    Compare descriptor vectors based on a similarity correlation.
+
+    Args:
+        u (ndarray): 1D array of type np.float32 containing image descriptors.
+        v (ndarray): 1D array of type np.float32 containing image descriptors.
+
+    Returns:
+        float: similarity score between 0 and 1.
+    """
+    return cv2.compareHist(u, v, cv2.HISTCMP_CORREL)
+
+
+def similarity_chi_square(u, v):
+    """
+    Compare descriptor vectors based on a similarity chi-square.
+
+    Args:
+        u (ndarray): Expected 1D array of type np.float32 containing image descriptors.
+        v (ndarray): Observed 1D array of type np.float32 containing image descriptors.
+
+    Returns:
+        float: similarity score between 0(equal) and >0(high values means more difference).
+    """
+
+    return cv2.compareHist(u, v, cv2.HISTCMP_CHISQR)
+
+
+def similarity_hellinguer(u, v):
+    """
+    Compare descriptor vectors based on a similarity hellinguer.
 
     Args:
         u (ndarray): 1D array of type np.float32 containing image descriptors.
@@ -10,4 +45,47 @@ def similarity(u, v):
         float: similarity score between 0 and 1.
     """
 
-    pass
+    return 1 - cv2.compareHist(u, v, cv2.HISTCMP_HELLINGER)
+
+
+def similarity_dominant_color(image_expected, image_observed, k=4, image_processing_size=None):
+    """
+    Compare descriptor vectors based on a similarity hellinguer.
+
+    Args:
+        image_expected (ndarray): (H x W x C) 3D array of type np.uint8 containing an expected image.
+        image_observed (ndarray): (H x W x C) 3D array of type np.uint8 containing an observed image.
+        k (int): Number of clusters.
+        image_processing_size (ndarray): Size to rescale images.
+
+    Returns:
+        float: similarity score between 0 and 1.
+    """
+
+    if image_processing_size is not None:
+        image_expected = cv2.resize(image_expected, image_processing_size, interpolation=cv2.INTER_AREA)
+        image_observed = cv2.resize(image_observed, image_processing_size, interpolation=cv2.INTER_AREA)
+
+        image_expected = image_expected.reshape((image_expected.shape[0] * image_expected.shape[1], 3))
+        image_observed = image_observed.reshape((image_observed.shape[0] * image_observed.shape[1], 3))
+
+    clt_expected = KMeans(n_clusters=k)
+    clt2_observed = KMeans(n_clusters=k)
+
+    labels_expected = clt_expected.fit_predict(image_expected)
+    labels_observed = clt2_observed.fit_predict(image_observed)
+
+    label_counts_expected = Counter(labels_expected)
+    label_counts_observed = Counter(labels_observed)
+
+    dominant_color_expected = clt_expected.cluster_centers_[label_counts_expected.most_common(1)[0][0]]
+    dominant_color_observed = clt2_observed.cluster_centers_[label_counts_observed.most_common(1)[0][0]]
+
+    dominant_color_expected = cv2.cvtColor(np.uint8([[dominant_color_expected]]), cv2.COLOR_RGB2Lab)
+    dominant_color_observed = cv2.cvtColor(np.uint8([[dominant_color_observed]]), cv2.COLOR_RGB2Lab)
+    dominant_color_expected = np.float32(dominant_color_expected)
+    dominant_color_observed = np.float32(dominant_color_observed)
+    dominant_color_expected  *= 1/255;
+    dominant_color_observed *= 1/255;
+
+    return 1 - np.linalg.norm(dominant_color_expected-dominant_color_observed)
