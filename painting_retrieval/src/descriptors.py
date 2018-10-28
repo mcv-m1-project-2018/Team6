Bugfix in histogram normalization + Add result generation function

painting_retrieval/src/descriptors.py
@@ -119,7 +119,7 @@ def ycrcb_histogram(image):
 
     bins = 256
 
-    imageYCrCb = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+    imageYCrCb = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
     histY = cv2.calcHist([imageYCrCb], [0], None, [bins], [0, 256]).ravel()
     histCr = cv2.calcHist([imageYCrCb], [1], None, [bins], [0, 256]).ravel()
     histCb = cv2.calcHist([imageYCrCb], [2], None, [bins], [0, 256]).ravel()
@@ -196,6 +196,7 @@ def dominant_colors_rgb(image, k=5):
 
     return clusters.ravel().astype(np.float32)
 
+
 def glcm_texture_features(image):
     """
     Extract texture descriptors of an image by extracting properites from
@@ -214,16 +215,16 @@ def glcm_texture_features(image):
 
     # Compute grey-level co-ocurrence matrix
     glcm = greycomatrix(imageGray, distances=[1],
-                        angles=[0, np.pi/8, np.pi/4, 3*np.pi/8],
+                        angles=[0, np.pi / 8, np.pi / 4, 3 * np.pi / 8],
                         levels=256, symmetric=True, normed=True)
 
     # Calculate texture properties of GLCM
-    contrast = greycoprops(glcm, 'contrast').mean(axis =1)
-    dissimilarity = greycoprops(glcm, 'dissimilarity').mean(axis =1)
-    homogeneity = greycoprops(glcm, 'homogeneity').mean(axis =1)
-    ASM = greycoprops(glcm, 'ASM').mean(axis =1)
-    energy = greycoprops(glcm, 'energy').mean(axis =1)
-    correlation = greycoprops(glcm, 'correlation').mean(axis =1)
+    contrast = greycoprops(glcm, 'contrast').mean(axis=1)
+    dissimilarity = greycoprops(glcm, 'dissimilarity').mean(axis=1)
+    homogeneity = greycoprops(glcm, 'homogeneity').mean(axis=1)
+    ASM = greycoprops(glcm, 'ASM').mean(axis=1)
+    energy = greycoprops(glcm, 'energy').mean(axis=1)
+    correlation = greycoprops(glcm, 'correlation').mean(axis=1)
 
     text_features = [contrast, dissimilarity, homogeneity, ASM, energy, correlation]
     return np.array(text_features, dtype=np.float32)
@@ -246,8 +247,6 @@ def gabor_descriptor(image):
         feats[k, 1] = filtered.var()
 
     return feats.ravel().astype(np.float32)
-
-
 
 
 def extract_descriptors(image, method):

painting_retrieval/src/distances.py
@@ -32,7 +32,7 @@ def correlation(u, v):
         float: distance between histograms.
     """
 
-    return cv2.compareHist(u, v, cv2.HISTCMP_CORREL)
+    return 1 - cv2.compareHist(u, v, cv2.HISTCMP_CORREL)
 
 
 def chi_square(u, v):
@@ -91,7 +91,7 @@ def intersection(u, v):
     Returns:
         float: distance between histograms.
     """
-    return cv2.compareHist(u, v, cv2.HISTCMP_INTERSECT)
+    return 1 - cv2.compareHist(u, v, cv2.HISTCMP_INTERSECT)
 
 
 def kl_divergence(u, v):
@@ -164,8 +164,8 @@ def cosine_distance(u, v):
 
 
 def compute_distance(u, v, metric):
-    u = u/np.linalg.norm(u)
-    v = u/np.linalg.norm(v)
+    u = u / np.linalg.norm(u)
+    v = v / np.linalg.norm(v)
     func = {
         'euclidean_distance': euclidean_distance,
         'l1_distance': l1_distance,